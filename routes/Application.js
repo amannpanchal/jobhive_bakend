db8d77df980aa5c807b70e5eb35d67d708e24c5b
Aman <128085412+amannpanchal@users.noreply.github.com>
2026-05-19 02:46:39 +0530

    Update Application.js

--- routes/Application.js
+++ routes/Application.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const { isAuthenticated } = require('../middleware/auth');
-const { employeeGetAllApplication, jobSeekerGetAllApplication, jobSeekerDeleteApplication, postApplication } = require('../controllers/applicationController');
+const express = require("express");
+
+const { isAuthenticated } = require("../middleware/auth");
+
+const {
+  employeeGetAllApplication,
+  jobSeekerGetAllApplication,
+  jobSeekerDeleteApplication,
+  postApplication,
+} = require("../controllers/applicationController");
+
 const router = express.Router();
-router.route("/employeeapplications").put(isAuthenticated, employeeGetAllApplication);
-router.route('/jobseekerapplications').put(isAuthenticated, jobSeekerGetAllApplication);
-router.route('/jobseekerdeleteapplication/:id').put(isAuthenticated,jobSeekerDeleteApplication)
-router.route('/postapplication').post(isAuthenticated,postApplication)
-router.route('/deleteapplication/:id').put(isAuthenticated, jobSeekerDeleteApplication);
+
+router
+  .route("/employeeapplications")
+  .get(isAuthenticated, employeeGetAllApplication);
+
+router
+  .route("/jobseekerapplications")
+  .get(isAuthenticated, jobSeekerGetAllApplication);
+
+router
+  .route("/postapplication")
+  .post(isAuthenticated, postApplication);
+
+router
+  .route("/deleteapplication/:id")
+  .delete(isAuthenticated, jobSeekerDeleteApplication);
+
 module.exports = router;

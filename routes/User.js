59d110c7bb57bc1ede879f083c3ce242be5945c5
Aman <128085412+amannpanchal@users.noreply.github.com>
2026-05-19 02:47:30 +0530

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const { register, login, logOut, myprofile, editProfile } = require('../controllers/userController');
-const { isAuthenticated } = require('../middleware/auth');
+const express = require("express");
+
+const {
+  register,
+  login,
+  logOut,
+  myprofile,
+  editProfile,
+} = require("../controllers/userController");
+
+const { isAuthenticated } = require("../middleware/auth");
+
 const router = express.Router();
-router.route('/register').post(register)
-router.route('/login').post(login)
-router.route('/logout').put( isAuthenticated  ,logOut)
-router.route('/profile').put( isAuthenticated  ,myprofile)
-router.route('/update').put(isAuthenticated, editProfile);
+
+// REGISTER
+router.route("/register").post(register);
+
+// LOGIN
+router.route("/login").post(login);
+
+// LOGOUT
+router.route("/logout").get(isAuthenticated, logOut);
+
+// GET PROFILE
+router.route("/profile").get(isAuthenticated, myprofile);
+
+// UPDATE PROFILE
+router.route("/update").put(isAuthenticated, editProfile);
+
 module.exports = router;
